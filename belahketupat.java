bae373aa08fe41e63da4478c0a7aebc950b95eaf
rotamitanian <59088402+rotamitanian@users.noreply.github.com>
2021-10-15 17:27:15 +0700

    Add files via upload

---
+++ belahketupat.java
@@ -0,0 +1,34 @@
+public class belahketupat {
+
+        public static void main(String[] args) {
+       
+       
+          for (int i=1;i<=4;i++){
+                    for(int j=4;j>=i;j--){
+                        System.out.print(" ");
+                    }
+                    for(int k=1;k<=i;k++){
+                        System.out.print("0");
+                    }
+                    for (int l=1;l<=i-1;l++){
+                        System.out.print("0");
+                    }
+                    System.out.println();
+                }
+          for (int i=1;i<=5;i++){
+                    for(int j=1;j<=i-1;j++){
+                        System.out.print(" ");
+                    }
+                    for(int z=5;z>=i;z--){
+                     System.out.print("0");
+                     }
+                    for(int k=4;k>=i;k--){
+                      System.out.print("0");
+                      }
+                    System.out.println("");
+                }
+       
+        }
+       
+    
+}
